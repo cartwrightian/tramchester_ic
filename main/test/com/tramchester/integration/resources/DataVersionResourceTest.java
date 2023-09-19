6a87d864e97f7b338213180ec3647c5e0f62b6bd
Ian Cartwright <cartwright.ian@gmail.com>
2023-09-19 10:54:44 +0100

    temp move webpack dep in gradle as running for unit tests. New data version

--- main/test/com/tramchester/integration/resources/DataVersionResourceTest.java
+++ main/test/com/tramchester/integration/resources/DataVersionResourceTest.java
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(DropwizardExtensionsSupport.class)
 public class DataVersionResourceTest {
 
-    public static final String version = "2023-09-18T03:46:14";
+    public static final String version = "2023-09-19T03:46:02";
 
     private static final IntegrationAppExtension appExtension = new IntegrationAppExtension(App.class,
             new ResourceTramTestConfig<>(DataVersionResource.class));
