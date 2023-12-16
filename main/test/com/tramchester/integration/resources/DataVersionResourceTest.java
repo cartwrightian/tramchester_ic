383b62391adcd24b1aa03086d63efa30281670ed
Ian Cartwright <cartwright.ian@gmail.com>
2023-12-16 09:44:16 +0000

    wire up cert and https, remove old infra spec, new data version

--- main/test/com/tramchester/integration/resources/DataVersionResourceTest.java
+++ main/test/com/tramchester/integration/resources/DataVersionResourceTest.java
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(DropwizardExtensionsSupport.class)
 public class DataVersionResourceTest {
 
-    public static final String version = "2023-12-15T02:45:08";
+    public static final String version = "2023-12-16T02:45:09";
 
     private static final IntegrationAppExtension appExtension = new IntegrationAppExtension(App.class,
             new ResourceTramTestConfig<>(DataVersionResource.class));
