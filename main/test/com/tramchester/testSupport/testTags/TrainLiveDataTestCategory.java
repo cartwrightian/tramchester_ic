9a674149f2737f753d7257828f6ac98908534187
Ian Cartwright <cartwright.ian@gmail.com>
2024-08-22 10:14:51 +0100

    make sure can easily smoke test rail live data download

---
+++ main/test/com/tramchester/testSupport/testTags/TrainLiveDataTestCategory.java
@@ -0,0 +1,15 @@
+package com.tramchester.testSupport.testTags;
+
+import org.junit.jupiter.api.Tag;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.METHOD, ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Tag("TrainLiveData")
+public @interface TrainLiveDataTestCategory {
+    // NOTE: these tests will fail if API key not available in env var OPENLDB_APIKEY
+}
