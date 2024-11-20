321e1b5c734ee32034578de7ffd7fc3a44576d51
Ian Cartwright <ian.cartwright@softwire.com>
2024-11-20 09:33:42 +0000

    new data version

--- main/test/com/tramchester/integration/resources/DataVersionResourceTest.java
+++ main/test/com/tramchester/integration/resources/DataVersionResourceTest.java
@@ -22,7 +22,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(DropwizardExtensionsSupport.class)
 public class DataVersionResourceTest {
 
-    public static final String version = "2024-11-19T02:45:48Z";
+    public static final String version = "2024-11-20T02:45:48Z";
 
     private static final IntegrationAppExtension appExtension = new IntegrationAppExtension(App.class,
             new ResourceTramTestConfig<>(DataVersionResource.class));
