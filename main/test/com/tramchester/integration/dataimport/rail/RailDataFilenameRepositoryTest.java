5d76cfee869fac800c73ddb1ee98fd73487d2d38
Ian Cartwright <ian.cartwright@softwire.com>
2026-08-19 09:50:33 +0100

    data update

--- main/test/com/tramchester/integration/dataimport/rail/RailDataFilenameRepositoryTest.java
+++ main/test/com/tramchester/integration/dataimport/rail/RailDataFilenameRepositoryTest.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @DataUpdateTest
 public class RailDataFilenameRepositoryTest {
 
-    public static final String CURRENT_VERSION = "925";
+    public static final String CURRENT_VERSION = "932";
 
     private static GuiceContainerDependencies componentContainer;
     private RailDataFilenameRepository repository;
